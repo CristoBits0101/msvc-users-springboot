a47c7981179596887987f813b831a299f0d1123f
Cristo Suárez <cristobits0101@gmail.com>
2025-03-20 21:12:31 +0000

    Add security filter chain configuration for API route authorization

--- src/main/java/com/cryfirock/msvc/users/msvc_users/security/SpringSecurityConfig.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/security/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.cryfirock.msvc.users.msvc_users.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -11,11 +14,35 @@ public class SpringSecurityConfig {
     /**
      * To make it a reusable component
      * 
+     * Allow password encryption
+     * 
      * @return BCryptPasswordEncoder instance
      */
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * To make it a reusable component
+     * 
+     * Authorizing access to API routes
+     * 
+     * @return BCryptPasswordEncoder instance
+     */
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/users")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .csrf(csrf -> csrf
+                        .disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
 }
