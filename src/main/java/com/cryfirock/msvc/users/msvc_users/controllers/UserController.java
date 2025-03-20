1a8598831f3c38ed65299c1b73611f85dc7a5016
Cristo Suárez <cristobits0101@gmail.com>
2025-03-20 18:29:49 +0000

    Enhance getUserById method comments for clarity on user retrieval and response behavior

--- src/main/java/com/cryfirock/msvc/users/msvc_users/controllers/UserController.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/controllers/UserController.java
@@ -59,10 +59,13 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        // Find user by id
         Optional<User> userOptional = userService.findById(id);
+
         if (userOptional.isPresent())
             // Return 200 OK if found, else throws an exception (possible 500)
             return ResponseEntity.ok(userOptional.orElseThrow());
+
         // Return 404 Not Found if user does not exist
         return ResponseEntity.notFound().build();
     }
