dca1904e0d8d65257dda751ea64943b178a207f3
Cristo Suárez <cristobits0101@gmail.com>
2025-03-21 22:57:22 +0000

    Reintroduce no-args constructor in User class and adjust JoinTable annotation formatting

--- src/main/java/com/cryfirock/msvc/users/msvc_users/entities/User.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/entities/User.java
@@ -31,12 +31,10 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
 @Entity
-@NoArgsConstructor
 @Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "username"))
 public class User {
 
@@ -92,11 +90,8 @@ public class User {
      * Relationship
      */
     @ManyToMany
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"),
-            uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
+            @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
     private List<Role> roles;
 
     /**
@@ -111,6 +106,13 @@ public class User {
      */
     @Embedded
     private Audit audit;
+
+    /**
+     * Constructors
+     */
+    public User() {
+        this.audit = new Audit();
+    }
 
     /**
      * Methods
