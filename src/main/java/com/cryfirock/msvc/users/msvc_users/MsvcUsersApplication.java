b635e0f17dbafae114e52971c9b0862d00ac0c97
Cristo Suárez <cristobits0101@gmail.com>
2025-03-22 19:05:57 +0000

    Add component scanning for validations in AppConfig and remove from MsvcUsersApplication

--- src/main/java/com/cryfirock/msvc/users/msvc_users/MsvcUsersApplication.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/MsvcUsersApplication.java
@@ -2,13 +2,8 @@ package com.cryfirock.msvc.users.msvc_users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-    "com.cryfirock.msvc.users.msvc_users",
-    "com.cryfirock.msvc.users.msvc_users.validations"
-})
 public class MsvcUsersApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/cryfirock/msvc/users/msvc_users/config/AppConfig.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/config/AppConfig.java
@@ -2,10 +2,15 @@ package com.cryfirock.msvc.users.msvc_users.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
+@ComponentScan(basePackages = {
+        "com.cryfirock.msvc.users.msvc_users",
+        "com.cryfirock.msvc.users.msvc_users.validations"
+})
 public class AppConfig {
 
     @Bean
