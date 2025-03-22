e098625141948a75c06798234bb21229ee41c39a
Cristo Suárez <cristobits0101@gmail.com>
2025-03-22 15:06:17 +0000

    Update JsonIgnoreProperties in User entity to include handler and hibernateLazyInitializer

--- src/main/java/com/cryfirock/msvc/users/msvc_users/entities/User.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/entities/User.java
@@ -95,7 +95,7 @@ public class User {
     @ManyToMany
     @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
             @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
-    @JsonIgnoreProperties({ "users" })
+    @JsonIgnoreProperties({ "users", "handler", "hibernateLazyInitializer" })
     private List<Role> roles;
 
     /**
