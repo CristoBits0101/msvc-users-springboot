c4d93343312d5ea737ea1c001f609c5d0291b966
Cristo Suárez <cristobits0101@gmail.com>
2025-03-23 12:07:07 +0000

    Implement user details service and add username lookup functionality

--- src/main/java/com/cryfirock/msvc/users/msvc_users/repositories/UserRepository.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/repositories/UserRepository.java
@@ -2,14 +2,20 @@ package com.cryfirock.msvc.users.msvc_users.repositories;
 
 import com.cryfirock.msvc.users.msvc_users.entities.User;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    // Check if data is already registered
     boolean existsByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
 
     boolean existsByUsername(String username);
+
+    // Find user by username for authentication
+    Optional<User> findByUsername(String username);
 
 }
---
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/services/JpaUserDetailsService.java
@@ -0,0 +1,54 @@
+package com.cryfirock.msvc.users.msvc_users.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cryfirock.msvc.users.msvc_users.entities.User;
+import com.cryfirock.msvc.users.msvc_users.models.AccountStatus;
+import com.cryfirock.msvc.users.msvc_users.repositories.UserRepository;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Find user by username
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        // Throw exception if user not found by username
+        if (optionalUser.isEmpty())
+            throw new UsernameNotFoundException(String.format("User with username %s does not exist", username));
+
+        // Create UserDetails object and populate it with user data
+        User user = optionalUser.orElseThrow();
+
+        // Populate authorities with user's roles
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        // Check if user is enabled
+        boolean isEnabled = user.getAccountStatus() == AccountStatus.ACTIVE;
+
+        // Return UserDetails object
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                isEnabled, true, true, true,
+                authorities);
+    }
+
+}
