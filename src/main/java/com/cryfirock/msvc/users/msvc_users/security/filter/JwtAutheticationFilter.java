4699fe80dd39ac8e395d680a65bd4e72fe00ffdb
Cristo Suárez <cristobits0101@gmail.com>
2025-03-23 16:46:23 +0000

    Add JWT authentication filter and dependencies for token generation

--- src/main/java/com/cryfirock/msvc/users/msvc_users/security/filter/JwtAutheticationFilter.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/security/filter/JwtAutheticationFilter.java
@@ -1,7 +1,5 @@
 package com.cryfirock.msvc.users.msvc_users.security.filter;
 
-import java.io.IOException;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,17 +11,42 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Jwts;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+// This filter authenticates users and generates tokens during the login process
 public class JwtAutheticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    /**
+     * Attributes
+     */
     private AuthenticationManager authenticationManager;
 
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    /**
+     * Constructors
+     * 
+     * @param authenticationManager
+     */
     public JwtAutheticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
 
+    /**
+     * Methods
+     */
     @Override
     public Authentication attemptAuthentication(
             HttpServletRequest request,
@@ -51,6 +74,37 @@ public class JwtAutheticationFilter extends UsernamePasswordAuthenticationFilter
 
         return authenticationManager.authenticate(authenticationToken);
 
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        User user = (User) authResult.getPrincipal();
+
+        String username = user.getUsername();
+
+        String token = Jwts
+                .builder()
+                .subject(username)
+                .signWith(SECRET_KEY)
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+        Map<String, String> body = new HashMap<>();
+
+        //
+        body.put("token", "token");
+        body.put("username", username);
+        body.put("message", String.format("", username));
+
+        //
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 
 }
