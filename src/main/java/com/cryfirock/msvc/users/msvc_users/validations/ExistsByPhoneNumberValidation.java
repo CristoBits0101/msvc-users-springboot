e785ea5344a85974e8bc2058296a040a221136e6
Cristo Suárez <cristobits0101@gmail.com>
2025-03-24 18:58:08 +0000

    Remove unnecessary blank lines in validation classes for cleaner code

--- src/main/java/com/cryfirock/msvc/users/msvc_users/validations/ExistsByPhoneNumberValidation.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/validations/ExistsByPhoneNumberValidation.java
@@ -9,7 +9,6 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Component;
 
 @Component
