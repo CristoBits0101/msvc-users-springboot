8f06934d8235ec1758b0fdeaeb7a07c9c9a3ae0d
Cristo Suárez <cristobits0101@gmail.com>
2025-03-25 10:47:01 +0000

    Add global exception handler and error model for custom error responses

---
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/controllers/HandlerExceptionController.java
@@ -0,0 +1,31 @@
+package com.cryfirock.msvc.users.msvc_users.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@RestControllerAdvice
+public class HandlerExceptionController {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e) {
+        // New error instance
+        Error error = new Error();
+
+        // Initialize the attributes
+        error.setDate(new Date());
+        error.setError("Api rest no encontrado");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        // Returns a custom 404 error
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+
+}
---
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/models/Error.java
@@ -0,0 +1,15 @@
+package com.cryfirock.msvc.users.msvc_users.models;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Error {
+
+    private String message;
+    private String error;
+    private int status;
+    private Date date;
+
+}
