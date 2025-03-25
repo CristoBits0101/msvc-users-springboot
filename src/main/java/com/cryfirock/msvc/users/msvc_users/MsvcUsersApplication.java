f4f24dfbcc1585737f5c6b73acc1fb20598ef18d
Cristo Suárez <cristobits0101@gmail.com>
2025-03-25 16:19:16 +0000

    Enable AspectJ auto proxy support in MsvcUsersApplication

--- src/main/java/com/cryfirock/msvc/users/msvc_users/MsvcUsersApplication.java
+++ src/main/java/com/cryfirock/msvc/users/msvc_users/MsvcUsersApplication.java
@@ -4,8 +4,10 @@ package com.cryfirock.msvc.users.msvc_users;
  * Dependencies
  */
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.boot.SpringApplication;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class MsvcUsersApplication {
 
